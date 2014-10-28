e614a44057aa8a317ae38b64b858b7f67d5f500c
Bryan Wiebe <bryan@hutzmedia.com>
2014-10-28 16:39:18 -0700

    Rename nme to lime.

--- templates/android/MainActivity.java
+++ templates/android/MainActivity.java
@@ -16,7 +16,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends org.haxe.nme.GameActivity {
+public class MainActivity extends org.haxe.lime.GameActivity {
 
 	protected void onActivityResult( int requestCode , int resultCode , Intent data ) {
 		Log.w("trace", "onActivityResult...");
