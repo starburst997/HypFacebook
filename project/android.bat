99168f5d3124bac33c73df8a30871cc29d4a6bd4
starburst <starburst@failsafegames.com>
2015-01-25 20:49:09 -0500

    Clean

--- project/android.bat
+++
@@ -1,3 +0,0 @@
-haxelib run hxcpp Build.xml -Dandroid
-move ..\ndll\Android\libhypfacebook.so ..\ndll\Android\libhypfacebook-v7.so
-haxelib run hxcpp Build.xml -Dandroid
--- project/android.sh
+++
@@ -1,5 +0,0 @@
-rm -rf "obj"
-echo "Compiling for armv6"
-haxelib run hxcpp Build.xml -Dandroid
-echo "compiling for armv7"
-haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7
